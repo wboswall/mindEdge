2a12f79bde528f88821155511e4a0e32cf7ea104
William Boswall <wboswall@live.com>
2023-05-04 02:45:49 -0300

    Adding project website to the repository for the first time.

---
+++ travelSite/engine.js
@@ -0,0 +1,13 @@
+/*
+ *@author: William E Boswall
+ *@date: 04/05/2023
+ *@description: This file will be the engine that runs the front end of the site. 
+ */
+// global scope - (on top)
+"use strict";
+
+// local scope - inside block
+
+  let greeting ="Hello World! From the Javascript/ECMAScript World!";
+
+console.log(greeting);
