57f0cdf40df35b99d7a9990e872f65534bf9c2f1
William Boswall <wboswall@live.com>
2023-05-27 19:53:22 -0300

    Commit Version 0.0.0.10

---
+++ travelSite/scripts/engine.js
@@ -0,0 +1,20 @@
+/*
+ *@author: William E Boswall
+ *@date: 04/05/2023
+ *@description: This file will be the engine that runs the front end of the site. 
+ *@license: not available
+ *@version: 0.0.0.0
+ */
+// global scope - (on top)
+"use strict";
+
+// local scope - inside block
+
+const greeting ="Hello World! From the Javascript/ECMAScript World!";
+
+console.log(greeting);
+function Declare (){
+let message = "Hello World";
+console.log(message);
+}
+Declare();
